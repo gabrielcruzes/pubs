ff690390d1528dcf20a0f00f6dd1c844e3efd84f
gabrielcruzes <77302743+gabrielcruzes@users.noreply.github.com>
2023-11-21 22:53:27 -0300

    Create ligaedesligabombva.ino

---
+++ ligaedesligabombva.ino
@@ -0,0 +1,22 @@
+// Declaração de variáveis
+const int relePin = A0; // Pino ao qual o relé está conectado
+
+// Função setup()
+void setup() {
+  // Configura o pino como saída
+  pinMode(relePin, OUTPUT);
+}
+
+// Função loop()
+void loop() {
+   // Aguarda 5 segundos
+  delay(5000);
+  // Aciona o relé
+  digitalWrite(relePin, HIGH);
+  // Aguarda 60 segundos
+  delay(60000);
+  // Desativa o relé
+  digitalWrite(relePin, LOW);
+  // Aguarda 8 horas segundos
+  delay(28800000);
+}
